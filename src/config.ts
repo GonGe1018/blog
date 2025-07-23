e3917cd500570eb597d3911dd0ec0029ee4e46ee
Chaeho Yu <chaeho1234@naver.com>
2025-07-24 00:08:26 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -63,7 +63,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/GonGe1018", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
